9549a2deb64f4cc8888bfe31511015d4221e8277
AtherActive <ikbensalty@gmail.com>
2023-09-10 17:17:28 +0200

    Initial Commit v3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: `@import "${path.join(__dirname, 'src/styles/functions.scss')}";
+    @import "${path.join(__dirname, 'src/styles/variables.scss')}";`
+  }
 }
 
 module.exports = nextConfig
